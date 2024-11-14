af7860549d7954a35de629bd1189bc038cae5e97
Полина Платонова <platonovaPE@outlook.com>
2024-11-14 14:40:58 +0500

    Добавьте файлы проекта.

---
+++ Tyuiu.PlatonovaPE.Sprint2.Review.V9.Lib/DataService.cs
@@ -0,0 +1,20 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint2;
+namespace Tyuiu.PlatonovaPE.Sprint2.Review.V9.Lib
+{
+    public class DataService : ISprint2Task7V9
+    {
+        public bool CheckDotInShadedArea(double x, double y)
+        {
+            bool res;
+            if ((y <= Math.Sin(x)) && (y <= 0.5) && (y >= 0))
+            {
+                res = true;
+            }
+            else
+            {
+                res = false;
+            }
+            return res;
+        }
+    }
+}
